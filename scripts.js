97c0ef7c99a2ce976551b5030ba47e3e19c58363
InnovateBham <admin@InnovateBhams-MacBook-Pro.local>
2021-06-11 12:10:09 -0500

    first classroom

---
+++ scripts.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var btn = document.getElementById("button")
+    var friends  = document.getElementById("friends");
+    btn.addEventListener('click', function () {
+        var friends = ["Goofy", "Shrek", "Charlie Angels", "Curious George", "Simba"];
+    var location = ["Bathroom", "Dollar Tree", "InnovateBham", "Zoo", "River", "White House", "Laundry Room", "Seven11", "Woods", "Forest"]
+    var weapons =["ButterKnife", "Spectular", "Coconut", "Chair", "Arm", "Feet", "Gun", "Crunchy Hair", "Hair Spray", "Cigarette", "Boat", "Toe Nail", "Clippers", "Guitar", "Shark", "Speaker", "Glass", "Heel", "Arrow", "Trash Can"]
+    alert ("I accuse Goofy, cleaning a butterknife in the bathrrom!")
+
+
+document.body.appendChild(h3)
+// button.addEventListener("click", function() {
+//     makeFunc(i)
+//     friends = ['Goofy', 'Shrek', 'Charlie Angels', 'Curious George', 'Simba'];
+// });
+
+for (var i = 0; i <100; i++){
+    let h3 = document.createElement("h3")
+    document.body.appendChild(h3)
+    console.log(friends.length)
+    var accusationtext = document.createTextNode("Accusation")
+     button.addEventListener("click", makeFunc(i))};
+    })
+})
+
+function AccusationAlert() {
+    h3.addEventListener("click",)
+    var btnClick = h3;
+
+    return function () {
+        alert("Clicked!")
+    }
+
+    // return buttonClick;
+}
+
+var myFunc = makeFunc();
+
+myFunc();
+            
+                       
+
+    
